6408f7bee6686acccf827f2ab2e0dabee29fcbba
firepick1 (localhost) <karl@firepick.org>
2017-08-25 13:13:33 -0700

    oyamist

--- scripts/oyamist-install.sh
+++ scripts/oyamist-install.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+echo -e "INSTALL\t: upgrading Raspbian operating system"
+sudo apt update
+sudo apt full-upgrade
+
 echo -e "INSTALL\t: installing Node V8"
 curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
