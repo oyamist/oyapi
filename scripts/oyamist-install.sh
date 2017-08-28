2ac65824c9cd37fd7de6b448f3c5e1940b8e0255
firepick1 (localhost) <karl@firepick.org>
2017-08-28 11:11:43 -0700

    motion

--- scripts/oyamist-install.sh
+++ scripts/oyamist-install.sh
@@ -3,5 +3,11 @@ echo -e "INSTALL\t: upgrading Raspbian operating system"
 sudo apt update
 sudo apt full-upgrade
 
+echo -e "INSTALL\t: installing webcam motion capture"
+sudo apt install motion
+sudo mkdir -p /var/log/motion
+sudo chown pi:pi /var/log/motion
+
 echo -e "INSTALL\t: installing Node V8"
 curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+
