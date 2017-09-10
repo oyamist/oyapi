cafe478f3a5aa73b65b2456f5def1b20b23cccb1
firepick1 (localhost) <karl@firepick.org>
2017-09-09 20:02:12 -0700

    install

--- scripts/oyamist-install.sh
+++ scripts/oyamist-install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-echo -e "INSTALL\t: upgrading Raspbian operating system"
-sudo apt update
-sudo apt full-upgrade
+echo -e "INSTALL\t: `date` setting up Raspberry Pi`
+
+scripts/opencv-install.sh
 
 echo -e "INSTALL\t: installing webcam motion capture"
 sudo apt install motion
@@ -11,3 +11,4 @@ sudo chown pi:pi /var/log/motion
 echo -e "INSTALL\t: installing Node V8"
 curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
 
+echo -e "INSTALL\t: `date` OyaMist Raspberry Pi setup copmleted"
