5e18df34bec93d8cd22081c4c1ac68e8b487e889
firepick1 (localhost) <karl@firepick.org>
2017-09-26 11:03:31 -0700

    ...cosmetic changes...

---
+++ scripts/mist.js
@@ -0,0 +1,37 @@
+#!/usr/bin/env node
+console.log("mist\t: Starting OyaMist bioreactor", new Date());
+
+var cycle = "30/60";
+process.argv.forEach((a,i) => {
+    if (a === '-c') {
+        cycle = process.argv[i+1] || cycle;
+    }
+});
+var cycleTokens = cycle.split("/");
+var onSec = Number(cycleTokens[0]) || 30;
+var offSec = Number(cycleTokens[1]) || 60;
+
+console.log(`mist\t: misting cycle on:${onSec}s off:${offSec}s`);
+
+const rpio = require('rpio');
+    console.log("TEST\t: GPIO 2 (Pin 3) LOW");
+    rpio.open(3, rpio.OUTPUT, rpio.LOW);
+    console.log("TEST\t: GPIO 3 (Pin 5) HIGH");
+    rpio.open(5, rpio.OUTPUT, rpio.HIGH);
+
+    var msLast = new Date();
+    var state = rpio.LOW;
+    setInterval(() => {
+        rpio.write(5, state);
+        if (state === rpio.HIGH) {
+            state = rpio.LOW;
+            console.log("TEST\t: GPIO 3 (Pin 5) LOW");
+        } else {
+            state = rpio.HIGH;
+            console.log("TEST\t: GPIO 3 (Pin 5) HIGH");
+        }
+    }, 5000);
+
+
+    console.log("TEST\t: GPIO blink test END");
+}
