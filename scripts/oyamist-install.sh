1f13dde2009683bdea38250eb970fd119ca57df3
firepick1 (localhost) <karl@firepick.org>
2017-10-12 23:14:41 -0700

    install

--- scripts/oyamist-install.sh
+++ scripts/oyamist-install.sh
@@ -6,6 +6,13 @@ if [ ! -e node_modules ]; then
     curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -o
     sudo apt install -y nodejs
     npm install
+	npm run build
+fi
+
+if [ ! -e /etc/rc2.d/S50oyapi ]; then
+	echo -e "INSTALL\t: configuring oyapi for autoload on boot"
+	sudo cp scripts/oyapi /etc/init.d/oyapi
+	sudo ln -s /etc/init.d/oyapi /etc/rc2.d/S50oyapi
 fi
 
 exit
