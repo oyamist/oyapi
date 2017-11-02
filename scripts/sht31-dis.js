ce212369e25d983c9077116c59b4d549319ecaea
firepick1 (localhost) <karl@firepick.org>
2017-11-02 13:18:59 -0700

    sht31

--- scripts/sht31-dis.js
+++ scripts/sht31-dis.js
@@ -1,9 +1,29 @@
 #!/usr/bin/env node
 const rpio = require('rpio');
 const Sensor = require("oya-vue").Sensor;
-var sht31 = new Sensor(Sensor.TYPE_SHT31_DIS);
+const winston = require('winston');
+var opts = Object.assign({
+	i2cWrite: (adr, buf)=>{
+		rpio.i2cBegin();
+		rpio.i2cSetBaudRate(10000);    /* 10kHz for 10m cables */
+		rpio.i2cSetSlaveAddress(adr);
+		var rc = rpio.i2cWrite(buf);
+		winston.info(`i2cWrite => ${rc}`);
+		rpio.i2cEnd();
+	},
+	i2cRead: (adr, inBuf)=>{
+		rpio.i2cBegin();
+		rpio.i2cSetBaudRate(10000);    /* 10kHz for 10m cables */
+		rpio.i2cSetSlaveAddress(adr);
+		rpio.msleep(15);
+		var rc = rpio.i2cRead(inBuf);
+		rpio.i2cEnd();
+	},
+}, Sensor.TYPE_SHT31_DIS);
 
-console.log("TEST\t: SHT31-DIS Temperature and Humidity Sensor Test");
+var sht31 = new Sensor(opts);
+
+winston.info("SHT31-DIS Temperature and Humidity Sensor Test");
 
 rpio.i2cBegin();
 rpio.i2cSetBaudRate(10000);    /* 10kHz for 10m cables */
@@ -12,23 +32,23 @@ rpio.i2cSetSlaveAddress(sht31.address);
 for (var ia in process.argv) {
 	if (process.argv[ia] === '-W') {
 		var rc = rpio.i2cWrite(Buffer(sht31.heater.on)); // heater on
-		console.log(`TEST\t: heater on i2cWrite => ${rc}`);
+		winston.info(`heater on i2cWrite => ${rc}`);
 		rpio.sleep(1);
 	}
 	if (process.argv[ia] === '-w') {
 		var rc = rpio.i2cWrite(Buffer(sht31.heater.off)); // heater off
-		console.log(`TEST\t: heater off i2cWrite => ${rc}`);
+		winston.info(`heater off i2cWrite => ${rc}`);
 		rpio.sleep(1);
 	}
 }
 
 // read data
 var rc = rpio.i2cWrite(Buffer(sht31.cmdRead));
-console.log(`TEST\t: read command i2cWrite => ${rc}`);
+winston.info(`read command i2cWrite => ${rc}`);
 var inBuf = Buffer.alloc(sht31.dataRead.length);
 rpio.msleep(15);
 var rc = rpio.i2cRead(inBuf);
-console.log(`TEST\t: response rc:${rc} inBuf:`, 
+winston.info(`response rc:${rc} inBuf:`, 
 	inBuf[0].toString(16), // Temp high
 	inBuf[1].toString(16), // Temp low
 	inBuf[2].toString(16), // CRC Temp
@@ -39,7 +59,13 @@ console.log(`TEST\t: response rc:${rc} inBuf:`,
 var data = sht31.parseData(inBuf);
 var temp = data.temp;
 var humidity = data.humidity;
-console.log(`TEST\t: RH:${humidity.toFixed(1)}% Temp:${temp.toFixed(1)}C ${(temp*1.8+32).toFixed(1)}F `);
+winston.info(`RH:${humidity.toFixed(1)}% Temp:${temp.toFixed(1)}C ${(temp*1.8+32).toFixed(1)}F `);
+
+sht31.read().then(r=>{
+	winston.info(`sht31.read() => ${JSON.stringify(r)}`);
+}).catch(e=>{
+	winston.error(e);
+});
 
 rpio.i2cEnd();
 
