dc91b61031563ed5973a292ea247e5b6eca907b4
firepick1 (localhost) <karl@firepick.org>
2017-11-02 13:37:18 -0700

    sensor

--- src/oyapi.js
+++ src/oyapi.js
@@ -9,6 +9,24 @@
     const path = require("path");
     const rpio = require("rpio");
 
+    function i2cWrite(adr, buf) {
+        rpio.i2cBegin();
+        rpio.i2cSetBaudRate(10000);    /* 10kHz for 10m cables */
+        rpio.i2cSetSlaveAddress(adr);
+        var rc = rpio.i2cWrite(buf);
+        winston.info(`i2cWrite => ${rc}`);
+        rpio.i2cEnd();
+    }
+
+    function i2cRead(adr, inBuf) {
+        rpio.i2cBegin();
+        rpio.i2cSetBaudRate(10000);    /* 10kHz for 10m cables */
+        rpio.i2cSetSlaveAddress(adr);
+        rpio.msleep(15);
+        var rc = rpio.i2cRead(inBuf);
+        rpio.i2cEnd();
+    }
+
 	var count = 0;
 	var curState = null;
 
@@ -16,45 +34,50 @@
         constructor(name = "test", opts = {}) {
             super(name, Object.assign({
                 srcPkg,
+                i2cWrite,
+                i2cRead,
             }, opts));
             var self = this;
             if (rpio) {
-                winston.info("rpio available. Setting up PiMoroni Automation Hat...");
-                var ahat = self.ahat = new PmiAutomation();
-                ahat.enable();
-                ahat.light.power.write(127);
-                ahat.light.comms.write(127);
-                ahat.light.warn.write(127);
-                ahat.light.power.enable();
-                ahat.light.comms.enable();
-                ahat.light.warn.enable();
-                setTimeout(() => ahat.light.power.enable(false), 500);
-                setTimeout(() => ahat.light.comms.enable(false), 1000);
-                setTimeout(() => ahat.light.warn.enable(false), 1500);
-                self.emitter.on(OyaPi.EVENT_RELAY, (value, pin) => {
-                    ahat.light.power.enable(value);
-                    rpio.open(pin, rpio.OUTPUT, value ? rpio.HIGH : rpio.LOW);
-                });
-                const pbtn = 37;
-                rpio.open(pbtn, rpio.INPUT, rpio.PULL_DOWN);
-                rpio.poll(pbtn, (pin) => {
-                    var pinState = rpio.read(pin);
-                    var state = pinState ? 'pressed' : 'released' ;
-                    if (curState !== state) {
-                        if (curState) {
-                            winston.info(`Priming mist system`);
-                            self.vessel.setCycle(OyaVessel.CYCLE_PRIME);
-                        }
-                        curState = state;
-                        ahat.light.comms.enable(pinState);
-                        count++;
-                    }
-                });
+                this.init_rpio();
             } else {
                 winston.info("rpio not available.");
             }
         }
 
+        init_rpio() {
+            winston.info("rpio available. Setting up PiMoroni Automation Hat...");
+            var ahat = self.ahat = new PmiAutomation();
+            ahat.enable();
+            ahat.light.power.write(127);
+            ahat.light.comms.write(127);
+            ahat.light.warn.write(127);
+            ahat.light.power.enable();
+            ahat.light.comms.enable();
+            ahat.light.warn.enable();
+            setTimeout(() => ahat.light.power.enable(false), 500);
+            setTimeout(() => ahat.light.comms.enable(false), 1000);
+            setTimeout(() => ahat.light.warn.enable(false), 1500);
+            self.emitter.on(OyaPi.EVENT_RELAY, (value, pin) => {
+                ahat.light.power.enable(value);
+                rpio.open(pin, rpio.OUTPUT, value ? rpio.HIGH : rpio.LOW);
+            });
+            const pbtn = 37;
+            rpio.open(pbtn, rpio.INPUT, rpio.PULL_DOWN);
+            rpio.poll(pbtn, (pin) => {
+                var pinState = rpio.read(pin);
+                var state = pinState ? 'pressed' : 'released' ;
+                if (curState !== state) {
+                    if (curState) {
+                        winston.info(`Priming mist system`);
+                        self.vessel.setCycle(OyaVessel.CYCLE_PRIME);
+                    }
+                    curState = state;
+                    ahat.light.comms.enable(pinState);
+                    count++;
+                }
+            });
+        }
     } //// class OyaPi
 
     module.exports = exports.OyaPi = OyaPi;
