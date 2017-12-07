ad7d5709598b7efcc8752e959874c954080629e8
Karl Lew <karl@firepick.org>
2017-12-07 04:39:13 -0800

    sql

--- scripts/create.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS sensordata (
-    vessel text NOT NULL,
-    evt text NOT NULL,
-    d text NOT NULL,
-    t text NOT NULL,
-    v real,
-    primary key(vessel,evt,d,t)
-) WITHOUT ROWID;
